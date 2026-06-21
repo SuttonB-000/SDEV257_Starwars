7a37aba7253f0ccc26e4d85d05126489e190054f
Sutton Bonte <sttnbonte0@gmail.com>
2026-06-21 16:10:13 -0400

    import navigation components

--- App.js
+++ App.js
@@ -1,20 +1,16 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+import * as React from "react";
+import { NavigatorContainer } from "@react-navigation/native";
+import{ createNativeStackNavigator } from "@react-navigation/native-stack";
+import Home from "./Home";
+import Settings from "./Settings";
+const Stack = createNativeStackNavigator(); //a funtion that setups navigation
+export default function app();
+   return (
+	<NavigationContainer>
+		<Stack.Navigator>
+	   		<Stack.Screen name="Home" component={Home} />
+	   		<Stack.Screen name="Settings"component={Settings}/>
+	   	<Stack.Navigator>
+	   </NavigationContainer>
+	);
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
