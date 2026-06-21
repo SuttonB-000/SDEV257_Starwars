82fd14d6d12c1c9c5fe7ba4c9b532cbcf4bdda52
Sutton Bonte <sttnbonte0@gmail.com>
2026-06-21 16:44:33 -0400

    fixed import typo, and added closing tag

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import * as React from "react";
-import { NavigatorContainer } from "@react-navigation/native";
+import { NavigationContainer } from "@react-navigation/native";
 import{ createNativeStackNavigator } from "@react-navigation/native-stack";
 import Home from "./Home";
 import Settings from "./Settings";
@@ -10,7 +10,7 @@ export default function app(){
 		<Stack.Navigator>
 	   		<Stack.Screen name="Home" component={Home} />
 	   		<Stack.Screen name="Settings"component={Settings}/>
-	   	<Stack.Navigator>
-	   </NavigationContainer>
+	   	</Stack.Navigator>
+	</NavigationContainer>
 	);
 }
