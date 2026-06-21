a137b305aa2f44b22acdf89c3c223f9d49e89542
Sutton Bonte <sttnbonte0@gmail.com>
2026-06-21 16:18:05 -0400

    added Home.js, initial commit

---
+++ Home.js
@@ -0,0 +1,12 @@
+import React from"react";
+import { View, Text, Button, Statusbar } from "react-native";
+import styles from "./styles";
+eport default function Home({ navigation }) {
+	return (
+		<View style-{styles.container}>
+			<StatusBar barStyle="dark-content"/>
+			<Text>Home Screen</Text>
+			<Button title="Settings" onPress={() =>navigation.navigate("Settings")} />
+			</View>
+	);
+}
