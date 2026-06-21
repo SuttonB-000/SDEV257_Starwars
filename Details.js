30e9d74a9394e9e71749ab528e4666bdacc22590
Sutton Bonte <sttnbonte0@gmail.com>
2026-06-21 19:04:47 -0400

    tornado inbound saving just in case

--- Details.js
+++ Details.js
@@ -1,2 +1,12 @@
 import React from "react";
-import { View, Text, StatusBar } from "react-native"
+import { View, Text, StatusBar } from "react-native";
+import styles from ".styles";
+export default function { route }) {
+	const { title } = route.params;
+	return (
+		<View style={styles.container}>
+			<StatusBar barStyle="dark-content" />
+			<Text>{title}</Text>
+		</View>
+	);
+}
