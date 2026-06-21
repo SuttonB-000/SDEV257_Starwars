76e64ea9c2e0e2d0948d320fc72b1ddae041c39b
Sutton Bonte <sttnbonte0@gmail.com>
2026-06-21 16:50:42 -0400

    clean up

--- Home.js
+++ Home.js
@@ -3,10 +3,10 @@ import { View, Text, Button, Statusbar } from "react-native";
 import styles from "./styles";
 export default function Home({ navigation }) {
 	return (
-		<View style-{styles.container}>
+		<View style={styles.container}>
 			<StatusBar barStyle="dark-content"/>
 			<Text>Home Screen</Text>
 			<Button title="Settings" onPress={() =>navigation.navigate("Settings")} />
-			</View>
+		</View>
 	);
 }
