e9a03132d377218628f76247d0728a45841019b4
Sutton Bonte <sttnbonte0@gmail.com>
2026-06-23 17:38:39 -0400

    restructure for snacks and clean up weird characters

--- Films.js
+++ Films.js
@@ -2,9 +2,9 @@ import React from "react";
 import { View, Text } from "react-native";
 import styles from "./styles";
 export default function Settings() {
-  return (
-    <View style={styles.container}>
-      <Text>Films</Text>
-    </View>
-  );
+return (
+    <View style={styles.container}>
+        <Text>Films</Text>
+    </View>
+    );
 }
